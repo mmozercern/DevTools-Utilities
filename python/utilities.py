9388debe1475f6365f513285fc6af8a00083c114
Devin Taylor <dntaylor@wisc.edu>
2016-06-29 03:42:44 -0500

    move out hdfs to utilities

--- python/utilities.py
+++ python/utilities.py
@@ -2,6 +2,7 @@ import os
 import sys
 import errno
 import operator
+import subprocess
 
 # common definitions
 ZMASS = 91.1876
@@ -70,3 +71,32 @@ def divWithError(num,denom):
     val = num[0]/denom[0] if denom[0] else 0.
     err = val * ((num[1]/num[0])**2 + (denom[1]/denom[0])**2)**0.5 if num[0] and denom[0] else 0.
     return (val, err)
+
+# hdfs functions
+def strip_hdfs(directory):
+    return '/'.join([x for x in directory.split('/') if x not in ['hdfs']])
+
+def hdfs_ls_directory(storeDir):
+    '''Utility for ls'ing /hdfs at UW'''
+    storeDir = strip_hdfs(storeDir)
+    command = 'gfal-ls srm://cmssrm2.hep.wisc.edu:8443/srm/v2/server?SFN=/hdfs/{0}'.format(storeDir)
+    out = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    if 'gfal-ls' in out:
+        log.error(out)
+        return []
+    return out.split()
+
+def get_hdfs_root_files(topDir,lastDir=''):
+    '''Utility for getting all root files in a directory (and subdirectories)'''
+    lsDir = strip_hdfs('{0}/{1}'.format(topDir,lastDir)) if lastDir else strip_hdfs(topDir)
+    nextLevel = hdfs_ls_directory(lsDir)
+    out = []
+    for nl in nextLevel:
+        if nl=='failed': # dont include
+            continue
+        elif nl[-4:]=='root': # its a root file
+            out += ['{0}/{1}'.format(lsDir,nl)]
+        else: # keep going down
+            out += get_hdfs_root_files(lsDir,nl)
+    return out
+
--- scripts/submit_job.py
+++ scripts/submit_job.py
@@ -30,7 +30,7 @@ try:
 except:
     crabLoaded = False
 
-from DevTools.Utilities.utilities import getJson
+from DevTools.Utilities.utilities import getJson, strip_hdfs, hdfs_ls_directory, get_hdfs_root_files
 
 
 def get_crab_workArea(args):
@@ -38,33 +38,6 @@ def get_crab_workArea(args):
     uname = os.environ['USER']
     scratchDir = 'data' if 'uwlogin' in gethostname() else 'nfs_scratch'
     return '/{0}/{1}/crab_projects/{2}'.format(scratchDir,uname,args.jobName)
-
-def strip_hdfs(directory):
-    return '/'.join([x for x in directory.split('/') if x not in ['hdfs']])
-
-def hdfs_ls_directory(storeDir):
-    '''Utility for ls'ing /hdfs at UW'''
-    storeDir = strip_hdfs(storeDir)
-    command = 'gfal-ls srm://cmssrm2.hep.wisc.edu:8443/srm/v2/server?SFN=/hdfs/{0}'.format(storeDir)
-    out = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    if 'gfal-ls' in out:
-        log.error(out)
-        return []
-    return out.split()
-
-def get_hdfs_root_files(topDir,lastDir):
-    '''Utility for getting all root files in a directory (and subdirectories)'''
-    lsDir = strip_hdfs('{0}/{1}'.format(topDir,lastDir))
-    nextLevel = hdfs_ls_directory(lsDir)
-    out = []
-    for nl in nextLevel:
-        if nl=='failed': # dont include
-            continue
-        elif nl[-4:]=='root': # its a root file
-            out += ['{0}/{1}'.format(lsDir,nl)]
-        else: # keep going down
-            out += get_hdfs_root_files(lsDir,nl)
-    return out
 
 def get_config(args):
     '''Get a crab config file based on the arguments of crabSubmit'''
