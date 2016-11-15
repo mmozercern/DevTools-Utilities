5549c73063f4dd2d409f8c725de43d6c5b77bdb8
Devin Taylor <dntaylor@wisc.edu>
2016-11-15 13:13:43 +0100

    more support

--- scripts/betterDAS.py
+++ scripts/betterDAS.py
@@ -4,6 +4,8 @@ import sys
 import time
 import logging
 import argparse
+import itertools
+import fnmatch
 
 logging.basicConfig(level=logging.INFO, stream=sys.stderr)
 
@@ -24,15 +26,47 @@ def client(args):
     dbsclient = DbsApi(url)
 
     if args.datasets:
+        datasets = []
         for dataset in args.datasets:
-            datasets = dbsclient.listDatasets(dataset=dataset)
-            for d in sorted([dataset['dataset'] for dataset in datasets]):
-                print d
+            datasets += dbsclient.listDatasets(dataset=dataset)
+        for d in sorted(datasets, key=lambda k: k['dataset']):
+            print d['dataset']
+
+    if args.primaryDatasets or args.acquisitionEras or args.processNames or args.dataTiers:
+        pds = args.primaryDatasets if args.primaryDatasets else ['']
+        aes = args.acquisitionEras if args.acquisitionEras else ['']
+        dts = args.dataTiers if args.dataTiers else ['']
+        datasets = []
+        for pd, ae, dt in itertools.product(pds, aes, dts):
+            kwargs = {}
+            if pd: kwargs['primary_ds_name'] = pd
+            if ae: kwargs['acquisition_era_name'] = ae
+            if dt: kwargs['data_tier_name'] = dt
+            if args.processNames: kwargs['detail'] = True
+            datasets += dbsclient.listDatasets(**kwargs)
+        for d in sorted(datasets, key=lambda k: k['dataset']):
+            if args.processNames:
+                if not any([fnmatch.fnmatch(d['processed_ds_name'],pn) for pn in args.processNames]): continue
+            print d['dataset']
+    
 
 def parse_command_line(argv):
     parser = argparse.ArgumentParser(description='Get information from DBS')
 
-    parser.add_argument('--datasets', type=str, nargs="*", help='Dataset names with wildcards')
+    subparsers = parser.add_subparsers(help='Return type')
+
+    # return a list of datasets
+    parser_dataset = subparsers.add_parser('dataset', help='Return a list of datasets.')
+
+    # full dataset path
+    full_dataset = parser_dataset.add_argument_group(description='Full dataset name. Dataset form: /[primaryDataset]/[processName]/[dataTier]')
+    full_dataset.add_argument('--datasets', type=str, nargs="*", help='Dataset names')
+    # partial paths
+    dataset_components = parser_dataset.add_argument_group(description='Dataset components')
+    dataset_components.add_argument('--primaryDatasets', type=str, nargs="*", default=[], help='Primary dataset names')
+    dataset_components.add_argument('--acquisitionEras', type=str, nargs="*", default=[], help='Acquisition era for datasets')
+    dataset_components.add_argument('--processNames', type=str, nargs="*", default=[], help='Process name')
+    dataset_components.add_argument('--dataTiers', type=str, nargs="*", default=[], help='Data tiers for dataset')
 
     return parser.parse_args(argv)
 
