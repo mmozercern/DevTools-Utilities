62c65f70d335229e17660d4552dd38d7e262208f
Devin Taylor <dntaylor@wisc.edu>
2016-11-16 13:07:02 +0100

    add sort order option

--- scripts/betterDAS.py
+++ scripts/betterDAS.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os
 import sys
 import time
@@ -25,12 +26,17 @@ def client(args):
     url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader'
     dbsclient = DbsApi(url)
 
+    sortType = {
+        'name': 'dataset',
+        'time': 'last_modification_date',
+    }
+
     if args.datasets:
         datasets = []
         for dataset in args.datasets:
-            datasets += dbsclient.listDatasets(dataset=dataset)
-        for d in sorted(datasets, key=lambda k: k['dataset']):
-            print d['dataset']
+            datasets += dbsclient.listDatasets(dataset=dataset,detail=True)
+        for d in sorted(datasets, key=lambda k: k[sortType[args.sortOrder]]):
+            print(d['dataset'])
 
     if args.primaryDatasets or args.acquisitionEras or args.processNames or args.dataTiers:
         pds = args.primaryDatasets if args.primaryDatasets else ['']
@@ -42,12 +48,12 @@ def client(args):
             if pd: kwargs['primary_ds_name'] = pd
             if ae: kwargs['acquisition_era_name'] = ae
             if dt: kwargs['data_tier_name'] = dt
-            if args.processNames: kwargs['detail'] = True
+            kwargs['detail'] = True
             datasets += dbsclient.listDatasets(**kwargs)
-        for d in sorted(datasets, key=lambda k: k['dataset']):
+        for d in sorted(datasets, key=lambda k: k[sortType[args.sortOrder]]):
             if args.processNames:
                 if not any([fnmatch.fnmatch(d['processed_ds_name'],pn) for pn in args.processNames]): continue
-            print d['dataset']
+            print(d['dataset'])
     
 
 def parse_command_line(argv):
@@ -67,6 +73,9 @@ def parse_command_line(argv):
     dataset_components.add_argument('--acquisitionEras', type=str, nargs="*", default=[], help='Acquisition era for datasets')
     dataset_components.add_argument('--processNames', type=str, nargs="*", default=[], help='Process name')
     dataset_components.add_argument('--dataTiers', type=str, nargs="*", default=[], help='Data tiers for dataset')
+
+    # sort order
+    parser_dataset.add_argument('--sortOrder', type=str, nargs='?', default='name', choices=['name','time'], help='Define output sort order')
 
     return parser.parse_args(argv)
 
